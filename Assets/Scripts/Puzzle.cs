a1ef339554aa43bdb62976c853de6ebbe51d4a78
Alex <up201705049@fe.up.pt>
2020-03-09 16:41:00 +0000

    Display completed (Some improvements necessary)

--- Assets/Scripts/Puzzle.cs
+++ Assets/Scripts/Puzzle.cs
@@ -3,7 +3,7 @@ using System;
 
 public enum TileType {Empty, Null, Red, Blue, Green, Yellow}
 
-public class Puzzle : ScriptableObject
+public class Puzzle
 {
     //HARDCODED PUZZLES//
     public static TileType[][] puzzle1 = new TileType[][]
@@ -24,8 +24,30 @@ public class Puzzle : ScriptableObject
         for(int i = 0; i < puzzleMatrix.Length; i++) {
             for(int j = 0; j < puzzleMatrix[i].Length; j++) {
                 if(puzzleMatrix[i][j] != TileType.Null) {
-                    Instantiate(tilePrefab, new Vector3(i, 0.125f, i), Quaternion.identity); //Base Block Below
-                    Instantiate(tilePrefab, new Vector3(i, 0.375f, i), Quaternion.identity); //Actual Tiles on top
+                    GameObject baseTile = UnityEngine.Object.Instantiate(tilePrefab, new Vector3(i, 0.125f, j), Quaternion.identity); //Base Block Below
+                    baseTile.GetComponent<MeshRenderer>().material.color = Color.white;
+                          
+                    if((int)puzzleMatrix[i][j] > 1) {
+                        GameObject instantiatedTile = UnityEngine.Object.Instantiate(tilePrefab, new Vector3(i, 0.375f, j), Quaternion.identity); //Actual Tiles on top                    
+
+                        switch(puzzleMatrix[i][j]) {
+                            case TileType.Blue:
+                                instantiatedTile.GetComponent<Renderer>().material.color = Color.blue;
+                            break;
+                            case TileType.Red:
+                                instantiatedTile.GetComponent<Renderer>().material.color = Color.red;
+                            break;
+                            case TileType.Yellow:
+                                instantiatedTile.GetComponent<Renderer>().material.color = Color.yellow;
+                            break;
+                            case TileType.Green:
+                                instantiatedTile.GetComponent<Renderer>().material.color = Color.green;
+                            break;
+                            default:
+                                Debug.LogError("Unknown Material for tile: " + puzzleMatrix[i][j]);                        
+                            break;
+                        }          
+                    }
                 }
             }
         }
