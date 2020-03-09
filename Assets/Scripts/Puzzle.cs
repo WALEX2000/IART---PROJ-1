2a685688609bd5f965e8554a94ec1cdb77272736
Alex <up201705049@fe.up.pt>
2020-03-09 14:41:40 +0000

    trying to initialise puzzle representation

--- Assets/Scripts/Puzzle.cs
+++ Assets/Scripts/Puzzle.cs
@@ -3,17 +3,18 @@ using System;
 
 public enum TileType {Empty, Null, Red, Blue, Green, Yellow}
 
-[CreateAssetMenu(fileName = "New Puzzle", menuName = "Puzzle")]
 public class Puzzle : ScriptableObject
 {
-    public TileTypeList[] puzzleMatrix;
+    private static TileType[][] puzzle1 =
+    {
+        {TileType.Red, TileType.Empty, TileType.Null},
+        {TileType.Empty, TileType.Empty, TileType.Blue},
+        {TileType.Null, TileType.Null, TileType.Empty}
+    };
+
+    public TileType[][] puzzleMatrix;
 
     public void generatePuzzle() {
 
     }
-}
-
-[Serializable]
-public class TileTypeList {
-    public TileType[] tileList;
 }
