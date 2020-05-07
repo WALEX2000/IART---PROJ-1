685604bae2c40343a6f3a1ce78d076b26d6a967e
Alex Carqueja <up201705049@fe.up.pt>
2020-05-07 17:36:05 +0100

    E já q estamos numa de fazer cenas a mais toma la este

--- src/Assets/Scripts/TrainingManager.cs
+++ src/Assets/Scripts/TrainingManager.cs
@@ -7,15 +7,10 @@ public class TrainingManager : MonoBehaviour
 {
     public GameObject tilePrefab;
     public List<TileType[][]> puzzleMatrixList = new List<TileType[][]>();
-
-    public Transform testTransform;
     public void Start() {
         puzzleMatrixList.Add(Example.puzzleEasy1);
         puzzleMatrixList.Add(Example.puzzle2);
         puzzleMatrixList.Add(Example.puzzleMedium);
-
-        Puzzle puzzle = generatePuzzle();
-        displayPuzzle(puzzle, testTransform);
     }
 
     public Puzzle generatePuzzle() {
