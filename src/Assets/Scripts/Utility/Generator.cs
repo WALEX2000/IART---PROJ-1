ca21bf2348a96d9835b207c348d9b4686eff3e0f
tiago-falves <tiago.falves98@gmail.com>
2020-05-18 20:29:01 +0100

    Almost there

--- src/Assets/Scripts/Utility/Generator.cs
+++ src/Assets/Scripts/Utility/Generator.cs
@@ -42,8 +42,10 @@ public class Generator
     public TileType[][] generatePuzzle(int size,int numberColors,int numberMoves){
  
         System.Random rnd = new System.Random();
-
         TileType[][] matrix = new TileType[size*2][];
+        //Get random color array
+        List<TileType> colors = getColorArray(numberColors);
+        List<List<(int,int)>> puzzle = new List<List<(int, int)>>();
         int begin = size - size / 2;
         int end = size + size / 2;
 
@@ -55,18 +57,17 @@ public class Generator
             matrix[i] = line;
         }
 
-        //Get random color array
-        List<TileType> colors = getColorArray(numberColors);
-
 
         int x = UnityEngine.Random.Range(begin, end);
         int y = UnityEngine.Random.Range(begin, end);
-
         int numberMovesFirstColor = numberMoves/numberColors;
         int initialSize = (int) Math.Pow(((size*size)/numberColors),1.0/numberMovesFirstColor);
+        
+        //Generates first color and adds it to puzzle
+        puzzle.Add(generateXAdjacentSqares(matrix,colors[0],initialSize,begin,end,x,y));
 
-
-        generateXAdjacentSqares(matrix,colors[0],initialSize,begin,end,x,y);
+        //Moves color some moves
+        moveColor(colors[0],numberMovesFirstColor,matrix);
 
 
         for (int i = 1; i < colors.Count; i++){
@@ -74,24 +75,53 @@ public class Generator
             int index = UnityEngine.Random.Range(0, adjacent.Count);
             x = adjacent[index].Item1;
             y = adjacent[index].Item2;
-            generateXAdjacentSqares(matrix,colors[i],initialSize,begin,end,x,y);
+            puzzle.Add(generateXAdjacentSqares(matrix,colors[i],initialSize,begin,end,x,y));
+            moveColor(colors[i],numberMovesFirstColor,matrix);
+        }
+        //Set other elements to null Matrix
+        for (int i = 0; i < size * 2 ; i++){
+            for (int j = 0; j < size * 2; j++){
+                if(matrix[i][j] == TileType.Empty)
+                    matrix[i][j] = TileType.Null;
 
+            }
+                
         }
         
         return matrix;
     }
 
+
+    private void moveColor(TileType color,int numberMoves,TileType[][] matrix){
+        int count = 0;
+        bool foundMove = true;
+        while(count < numberMoves && foundMove){
+            foundMove = true;
+            if(moveUp(color,matrix)) count++;
+            else if(moveDown(color,matrix)) count++;
+            else if(moveLeft(color,matrix)) count++;
+            else if(moveRight(color,matrix)) count++;
+            else{foundMove = false;}
+        }
+    }
+
     //Gera x Quadrados adjacentes
-    private void generateXAdjacentSqares(TileType[][] matrix,TileType tile, int number,int begin,int end,int i,int j){
+    //Retorna as posiçoes dos quadrados gerados
+    private List<(int,int)> generateXAdjacentSqares(TileType[][] matrix,TileType tile, int number,int begin,int end,int i,int j){
+        List<(int,int)> result = new List<(int, int)>();
         List<(int,int)> adjacent = new List<(int, int)>();
         for (int k = 0; k < number; k++)
         {
             matrix[i][j] = tile;  
-            adjacent = getAdjacent(i,j,matrix,begin,end); //Ao usar isto ele vai estar a procurar em filinha, a alternativa e percorrer tudo
+            result.Add((i,j));
+            //adjacent = getAdjacent(i,j,matrix,begin,end); //Ao usar isto ele vai estar a procurar em filinha, a alternativa e percorrer tudo
+            adjacent = getAdjacent(tile,matrix,begin,end); //Percorre a matriz toda
             int index = UnityEngine.Random.Range(0, adjacent.Count);
             i = adjacent[index].Item1;
             j = adjacent[index].Item2;  
         }
+
+        return result;
     }
 
     //Get adjacent Empty positions to a position
@@ -170,13 +200,7 @@ public class Generator
     }
 
 
-    /*public void unfold(TileType[][] matrix){
-        List<TileType> colors = puzzleColors(matrix);
-        foreach (TileType color in colors){
-            int symetricAxis = canUnfoldUp(matrix,color);
-            if(symetricAxis  != -1) undoMoveUp(color,matrix);
-        }      
-    }*/
+    
 
     
 
@@ -352,4 +376,219 @@ public class Generator
         
         return matrix;
     }
+
+
+    public bool moveUp(TileType tile,TileType[][] puzzleMatrix)
+    {
+        int rotationAxis = puzzleMatrix.Length;
+        bool foundAxis = false;
+
+        //Discover the rotation axis
+        for (int i = 0; (i < puzzleMatrix.Length) && (!foundAxis); i++)
+        {
+            for (int j = 0; j < puzzleMatrix[i].Length; j++)
+            {
+                if (puzzleMatrix[i][j] == tile)
+                {
+                    rotationAxis = i;
+                    foundAxis = true;
+                    break;
+                }
+            }
+        }
+
+        List<Tuple<int, int>> positions = new List<Tuple<int, int>>();
+
+        //Discovers if all the elements can rotate
+        //Adds positions of the tiles to a List
+        for (int i = rotationAxis; i < puzzleMatrix.Length; i++)
+        {
+            for (int j = 0; j < puzzleMatrix[i].Length; j++)
+            {
+                if (tile == puzzleMatrix[i][j])
+                {
+                    int symetricX = i - 2 * (i - rotationAxis) - 1;
+                    if (canMove(symetricX, j,puzzleMatrix)){
+                        positions.Add(Tuple.Create(symetricX, j));
+                    }
+                    else return false;
+                }
+            }
+        }
+
+        //If they can all move, change the colors
+        for (int i = 0; i < positions.Count; i++){
+            puzzleMatrix[positions[i].Item1][positions[i].Item2] = tile;
+        }
+
+        return true;
+    }
+
+    public bool moveDown(TileType tile,TileType[][] puzzleMatrix)
+    {
+        int rotationAxis = -1;
+        bool foundAxis = false;
+
+        //Discover the rotation axis
+        for (int i = puzzleMatrix.Length - 1; (i >= 0) && (!foundAxis); i--)
+        {
+            for (int j = 0; j < puzzleMatrix[i].Length; j++)
+            {
+                if (puzzleMatrix[i][j] == tile)
+                {
+                    rotationAxis = i;
+                    foundAxis = true;
+                    break;
+                }
+            }
+        }
+
+        List<Tuple<int, int>> positions = new List<Tuple<int, int>>();
+
+        //Discovers if all the elements can rotate
+        //Adds positions of the tiles to a List
+        for (int i = 0; i <= rotationAxis; i++)
+        {
+            for (int j = 0; j < puzzleMatrix[i].Length; j++)
+            {
+                if (tile == puzzleMatrix[i][j])
+                {
+                    int symetricX = i + 2 * (rotationAxis - i) + 1;
+                    if (canMove(symetricX, j,puzzleMatrix))
+                    {
+                        positions.Add(Tuple.Create(symetricX, j));
+                    }
+                    else return false;
+                }
+            }
+        }
+
+        //If they can all move, change the colors
+        for (int i = 0; i < positions.Count; i++)
+        {
+            puzzleMatrix[positions[i].Item1][positions[i].Item2] = tile;
+        }
+
+        return true;
+    }
+
+    public bool moveLeft(TileType tile,TileType[][] puzzleMatrix)
+    {
+
+        int rotationAxis = puzzleMatrix.Length;
+
+        bool foundAxis = false;
+
+        //Discover the rotation axis
+        for (int j = 0; (j < puzzleMatrix[0].Length) && (!foundAxis); j++)
+        {
+            for (int i = 0; i < puzzleMatrix.Length; i++)
+            {
+                if (puzzleMatrix[i][j] == tile)
+                {
+                    rotationAxis = j;
+                    foundAxis = true;
+                    break;
+                }
+            }
+        }
+
+        List<Tuple<int, int>> positions = new List<Tuple<int, int>>();
+
+        //Discovers if all the elements can rotate
+        //Adds positions of the tiles to a List
+        for (int i = 0; i < puzzleMatrix.Length; i++)
+        {
+            for (int j = rotationAxis; j < puzzleMatrix[i].Length; j++)
+            {
+                if (tile == puzzleMatrix[i][j])
+                {
+                    int symetricY = j - 2 * (j - rotationAxis) - 1;
+                    if (canMove(i, symetricY,puzzleMatrix))
+                    {
+                        positions.Add(Tuple.Create(i, symetricY));
+                    }
+                    else return false;
+                }
+            }
+        }
+
+        //If they can all move, change the colors
+        for (int i = 0; i < positions.Count; i++)
+        {
+            puzzleMatrix[positions[i].Item1][positions[i].Item2] = tile;
+        }
+
+        return true;
+    }
+
+    public bool moveRight(TileType tile,TileType[][] puzzleMatrix)
+    {
+        int rotationAxis = -1;
+        bool foundAxis = false;
+
+        //Discover the rotation axis
+        for (int j = puzzleMatrix[0].Length - 1; (j >= 0) && (!foundAxis); j--)
+        {
+            for (int i = 0; i < puzzleMatrix.Length; i++)
+            {
+                if (puzzleMatrix[i][j] == tile)
+                {
+                    rotationAxis = j;
+                    foundAxis = true;
+                    break;
+                }
+            }
+        }
+
+        List<Tuple<int, int>> positions = new List<Tuple<int, int>>();
+
+        //Discovers if all the elements can rotate
+        //Adds positions of the tiles to a List
+        for (int i = 0; i < puzzleMatrix.Length; i++)
+        {
+            for (int j = 0; j <= rotationAxis; j++)
+            {
+                if (tile == puzzleMatrix[i][j])
+                {
+                    int symetricY = j + 2 * (rotationAxis - j) + 1;
+                    if (canMove(i, symetricY,puzzleMatrix))
+                    {
+                        positions.Add(Tuple.Create(i, symetricY));
+                    }
+                    else return false;
+                }
+            }
+        }
+
+        //If they can all move, change the colors
+        for (int i = 0; i < positions.Count; i++)
+        {
+            puzzleMatrix[positions[i].Item1][positions[i].Item2] = tile;
+        }
+
+        return true;
+    }
+  
+
+    public bool canMove(int i, int j,TileType[][] puzzleMatrix)
+    {
+        if (i < puzzleMatrix.Length && i >= 0)
+        {
+            if (j >= 0 && j < puzzleMatrix[i].Length)
+            {
+                if (puzzleMatrix[i][j] == TileType.Empty) return true;
+            }
+        }
+        return false;
+    }
+
+
+    /*public void unfold(TileType[][] matrix){
+        List<TileType> colors = puzzleColors(matrix);
+        foreach (TileType color in colors){
+            int symetricAxis = canUnfoldUp(matrix,color);
+            if(symetricAxis  != -1) undoMoveUp(color,matrix);
+        }      
+    }*/
 }
